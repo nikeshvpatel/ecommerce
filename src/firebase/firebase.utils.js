7d6392ea7b6e4f327cefbc414f1f9d415bd1c8f0
nikeshvpatel <ptlnik13@gmail.com>
2021-11-13 14:35:26 -0500

    106. Google Single Sign On Authentication and store user in Database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,25 @@ const config = {
     measurementId: "G-8VXCW34JXN"
 }
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapshot = await userRef.get();
+    if (!snapshot.exists) {
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({displayName, email, createdAt, ...additionalData})
+
+        } catch (e) {
+            console.log(e.message);
+        }
+
+    }
+    return userRef;
+}
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
