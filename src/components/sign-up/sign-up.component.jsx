cbc5c2bf6186856c5b3b7e1c3001d96cff5f8033
nikeshvpatel <ptlnik13@gmail.com>
2021-11-14 14:32:24 -0500

    111. Complete Sing in function with Google Sign in and individual email/password

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -55,14 +55,10 @@ class SignUp extends React.Component {
                 <h2 className="title">I do not have an account</h2>
                 <span>Sign up with your email and password</span>
                 <form onSubmit={this.handleSubmit} className="sign-up-form">
-                    <FormInput type='text' name='displayName' value={displayName} onChange={this.handleChange}
-                               label='Display Name' required/>
-                    <FormInput type='email' name='email' value={email} onChange={this.handleChange}
-                               label='Email' required/>
-                    <FormInput type='text' name='password' value={password} onChange={this.handleChange}
-                               label='Password' required/>
-                    <FormInput type='text' name='confirmPassword' value={confirmPassword} onChange={this.handleChange}
-                               label='Confirm Password' required/>
+                    <FormInput type='text' name='displayName' value={displayName} onChange={this.handleChange} label='Display Name' required/>
+                    <FormInput type='email' name='email' value={email} onChange={this.handleChange} label='Email' required/>
+                    <FormInput type='password' name='password' value={password} onChange={this.handleChange} label='Password' required/>
+                    <FormInput type='password' name='confirmPassword' value={confirmPassword} onChange={this.handleChange} label='Confirm Password' required/>
                     <CustomButton type='submit'>Sign Up</CustomButton>
                 </form>
             </div>
