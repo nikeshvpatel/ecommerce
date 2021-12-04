28815a4be3d543f465097ddcfc8b9b21a44049d0
nikeshvpatel <ptlnik13@gmail.com>
2021-12-03 22:29:00 -0500

    129. Cart Item Component

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -3,7 +3,7 @@ import {addItemToCart} from "./cart.utills";
 
 const INITIAL_STATE = {
     hidden: true,
-    cartItem: []
+    cartItems: []
 }
 
 const CartReducer = (state = INITIAL_STATE, action) => {
@@ -16,7 +16,7 @@ const CartReducer = (state = INITIAL_STATE, action) => {
         case CartActionTypes.ADD_ITEM:
             return {
                 ...state,
-                cartItem: addItemToCart(state.cartItem, action.payload)
+                cartItems: addItemToCart(state.cartItems, action.payload)
             }
         default:
             return state
