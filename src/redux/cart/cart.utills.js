28b203922a2b9a8601ba7b197eb49e71e8c41a10
nikeshvpatel <ptlnik13@gmail.com>
2021-12-06 21:06:42 -0500

    142. Remove items at checkout

--- src/redux/cart/cart.utills.js
+++ src/redux/cart/cart.utills.js
@@ -5,3 +5,11 @@ export const addItemToCart = (cartItems, cartItemsToAdd) => {
     }
     return [...cartItems, {...cartItemsToAdd, quantity: 1}];
 }
+
+export const removeItemsFromCart = (cartItems, cartItemsToRemove) => {
+    const existingCartItem = cartItems.find(cartItem => cartItem.id === cartItemsToRemove.id);
+    if (existingCartItem.quantity === 1) {
+        return cartItems.filter(cartItem => cartItem.id !== cartItemsToRemove.id)
+    }
+    return cartItems.map(cartItem => cartItem.id === cartItemsToRemove.id ? {...cartItem, quantity: cartItem.quantity - 1} : cartItem)
+}
